6830ae9cfb15cbf2d8eab468c5e394ce99795bc2
diegorezm <dmourarezende@gmail.com>
2026-01-17 17:07:58 -0300

    index page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,27 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Link, createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
 
-import { SymptomForm } from '@/components/patient/symptom-form'
+export const Route = createFileRoute('/')({
+  component: IndexPage,
+})
 
-export const Route = createFileRoute('/')({ component: App })
-
-function App() {
+function IndexPage() {
   return (
-    <div className="max-w-4xl mx-auto mt-6">
-      <SymptomForm
-        onSubmit={(v) => {
-          console.log(v)
-        }}
-      />
+    <div className="max-w-4xl mx-auto mt-12 text-center space-y-6">
+      <h1 className="text-3xl font-bold text-primary">Bem-vindo ao VivaBem</h1>
+      <p className="text-muted-foreground">
+        Acompanhe seus sintomas e compartilhe atualizações com seu médico em
+        tempo real.
+      </p>
+
+      <div className="flex justify-center gap-4">
+        <Button>
+          <Link to="/paciente">Entrar como Paciente</Link>
+        </Button>
+        <Button variant="outline">
+          <Link to="/doutor">Entrar como Médico</Link>
+        </Button>
+      </div>
     </div>
   )
 }
